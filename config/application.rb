2588d35036b7081580e4d4cf5d782877d1d8993c
Bob Ershov <raveman@raveman.ru>
2014-08-23 21:19:46 +0400

    app config: assets precompile added images

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module MobileRuseminar
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.i18n.default_locale = :ru
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
